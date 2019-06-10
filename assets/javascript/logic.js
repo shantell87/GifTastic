96c2eb05a93b6d5551f4bedbba4a6e0efee1d479
Shantell Davis <shantell87@gmail.com>
2019-06-09 19:36:37 -0700

    rendered initial buttons and connected git url functions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,67 +1,71 @@
-//Initial array for artists
+$(function(){
+    renderButtons(artists, '.artist-btn', '#buttonsView')
+})
 var artists = ["Adele", "Michael Jackson"];
 
-//capture artist name function
-function alertartistName(){
-}
 
-//function for displaying artists input
-//Deleting previous input prior to new input. No repeats
-function renderButtons() {
-    $("#buttons-view").empty();
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayArtistInfo() {
 
-    //loop through the array of artists
-for (var i = 0; i < artists.length; i++) {
-    var a = $("<button>");
-    // Adding a class
-    a.addClass("artist");
-    // Added a data-attribute
-    a.attr("data-name", artists[i]);
-    // Provided the initial button text
-    a.text(artists[i]);
-    // Added the button to the HTML
-    $("#buttons-view").append(a); 
-}
-}
-
-//click button function
-$("#add-artist").on("click", function(event){
-    event.preventDefault();
-//grabs the input from the textbox; adding button array
-    var artist = $("#artists-input").val().trim();
-    artists.push(artist);
-    renderButtons();
-})
-$(document).on("click", ".artist", alertartistName);
-renderButtons();
-
-$("button").on("click", function() {
-    var person = $(this).attr("data-name");
+    var artist = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3";
+    artists + "&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3";;
 
+    // Creates AJAX call for the specific movie button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        var results = response.data;
+    }).then(function (response) {
+        const $div = $('<div>').addClass('artist');
+        const $p1 = $('<p>').text(`Rating: ${response.Rated}`);
+       
+        $div.append($p1);
+        $('#gifsview').prepend($div);
+    });
 
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div>");
+}
 
-           var rating = results[i].rating;
+// Function for displaying movie data
+function renderButtons() {
 
-          var p = $("<p>").text("Rating: " + rating);
+    // Deletes the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttonsView").empty();
 
-          var artistImage = $("<img>");
-          artistImage.attr("src", results[i].images.fixed_height.url);
+    // Loops through the array of movies
+    for (var i = 0; i < artists.length; i++) {
 
-          gifDiv.prepend(p);
-          gifDiv.prepend(artistImage);
+        // Then dynamicaly generates buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adds a class of movie to our button
+        a.addClass("artist-btn");
+        // Added a data-attribute
+        a.attr("data-name", artists[i]);
+        // Provided the initial button text
+        a.text(artists[i]);
+        // Added the button to the buttons-view div
+        $("#buttonsView").append(a);
+    }
+}
 
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
-      });
-  
+// This function handles events where the add movie button is clicked
+$("#add-artist").on("click", function (event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var artist = $("#artists-input").val().trim();
 
-})
+    // The movie from the textbox is then added to our array
+    artists.push(artist);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+
+});
+
+// Adding click event listeners to all elements with a class of "movie"
+$(document).on("click", ".artist-btn", displayArtistInfo);
+
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
