b268a678b714b05b010785bb1e1330b227997492
Shantell Davis <shantell87@gmail.com>
2019-06-09 21:43:42 -0700

    added renderButton functions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,29 +1,36 @@
-$(function(){
-    renderButtons(artists, '.artist-btn', '#buttonsView')
-})
+// $(function(){
+//     renderButtons(artists, '.person-btn', '#buttonsView')
+
 var artists = ["Adele", "Michael Jackson"];
 
-
 // displayMovieInfo function re-renders the HTML to display the appropriate content
-function displayArtistInfo() {
+$("button").on("click", function() {
 
-    var artist = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    artists + "&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3";;
+    var person = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3";
 
     // Creates AJAX call for the specific movie button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
-        const $div = $('<div>').addClass('artist');
-        const $p1 = $('<p>').text(`Rating: ${response.Rated}`);
-       
-        $div.append($p1);
-        $('#gifsview').prepend($div);
-    });
+    })
+    .then(function (response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
 
-}
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $('#gifsView').prepend(gifDiv);
+        }
+    });
+});
 
 // Function for displaying movie data
 function renderButtons() {
@@ -39,7 +46,7 @@ function renderButtons() {
         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
         // Adds a class of movie to our button
-        a.addClass("artist-btn");
+        a.addClass("person-btn");
         // Added a data-attribute
         a.attr("data-name", artists[i]);
         // Provided the initial button text
@@ -64,7 +71,7 @@ $("#add-artist").on("click", function (event) {
 });
 
 // Adding click event listeners to all elements with a class of "movie"
-$(document).on("click", ".artist-btn", displayArtistInfo);
+$(document).on("click", ".person-btn", displayArtistInfo);
 
 
 // Calling the renderButtons function to display the intial buttons
