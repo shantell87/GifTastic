b95e63e8cc9bcbb93fcc1cec2f301fdf5e46a6d0
Shantell Davis <shantell87@gmail.com>
2019-06-18 22:03:43 -0700

    form is producing buttons

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,82 +1,85 @@
-// $(function () {
-//     renderButtons(artists, 'searchButton','#buttonsView');
-// })
-
+//variables array
 var artists = ["Adele", "Michael Jackson"];
 
-//capture artist name function
-//function alertArtistName() {
-function displayArtistInfo(){
-    
-    var person = $(this).attr("data-person");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      person + "&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3";
+//create buttons for the array
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        var results = response.data;
-
-            // for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>").addClass('artist');
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
-                var image = $('<img>');
-                image.attr("src", results[i].images.fixed_height.url);
-                image.attr("data-animate", results[i].images.fixed_height.url);
-                image.attr("data-still", results[i].images.fixed_height_still.url);
-                image.attr("data-state", 'still');
-                gifDiv.append(p);
-                gifDiv.append(image);
-                $('#gifs').append(gifDiv);
-              
-            // }
-        })
-}
-
-//buttons for displaying artists
 function renderButtons() {
-    $("#buttonsView").empty();
+    $('#buttonArea').empty();
 
-    //loops through array of artists
+//function for appending search items to the array
+//create new buttons in buttonsArea
+
+   //loops through array of artists
     for (var i = 0; i < artists.length; i++) {
+        
         var a = $('<button>');
         // Adds a class of artist to our button
-        a.addClass('artist');
+        //a.addClass(artist);
         // Added a data-attribute
-        a.attr('data-name', artists[i]);
+        //a.attr('data-name', artists[i]);
+        a.attr("type", "button");
         // Provided the initial button text
         a.text(artists[i]);
         // Added the button to the buttons-view div
-        $("#buttonsView").append(a);
+        $("#buttonArea").append(a);
     }
 }
+renderButtons();
 
-//add new buttons when an artist is searched and submitted from the form
-$('#add-artist').on('click', function (event) {
-    event.prependDefault();
-    var artist = $("#artist-input").val().trim();
-    artists.push(artist);
+//when artist button is pushed
+$('#add-artist').on("click", function(event){
+    event.preventDefault();
+    var action = $('#artist-input').val().trim();
+    artists.push(action);
+
     renderButtons();
 });
- // Adding click event listeners to all elements with a class of "artist"
- $(document).on("click", ".artist", displayArtistInfo);
 
+$(document).on('click', '.artist', displayArtistGif);
 
- // Calling the renderButtons function to display the intial buttons
- renderButtons();
+//giphy api pull
+function displayArtistGif(){
+    var search = $(this).text();
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q="${search}"&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3`;
+    //`https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+//select buttons in array to produce giphys
+.then (function(response) {
+    for (var i = 0; i < response.data.length; i++) {
+        var gifDiv = $("<div>").addClass('artist');
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var animated = response.data[i].images.fixed_height.url;
+        var still = response.data[i].images.fixed_height_still.url;
+        var image = $('<img>');
+        image.attr("src", still);
+        image.attr("data-still", still);
+        image.attr("src", animated);
+        image.attr("data-animate", animated);
+        image.addClass('searchImage');
+        gifDiv.append(p);
+        gifDiv.append(image);
+        
+        //dump the giphy in #gifs area
+        $('.giphyView').append(gifDiv);
+      }
+})
+}
 
-
-//image animation on click and still when unclicked
-$(document).on('click', '.searchImage', function () {
-    var state = $(this).attr('data-state');
-    //switching from an animated state to a still state
-    if (state == 'still') {
+//animate/still giphy actions
+$('.giphyView').on('click', 'img',function(){
+    var state =$(this).attr('data-state');
+    if(state == 'still'){
         $(this).attr('src', $(this).data('animated'));
-        $(this).attr('data-state', 'animated');
+        $(this).attr('data-state','animated');
     } else {
         $(this).attr('src', $(this).data('still'));
-        $(this).attr('data-state', 'still');
+        $(this).attr('data-state','still'); 
     }
 })
+
+
+
