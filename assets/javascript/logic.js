213675b2e4ad5fa8f5235016bce44d6a0a7deaf6
Shantell Davis <shantell87@gmail.com>
2019-06-22 23:53:06 -0700

    fixed search variable in order to pull the correct giphy search term

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,43 @@
 //variables array
-var artists = ["Adele", "Michael Jackson"];
+var artists = ["adele", "michael jackson"];
+
+//giphy api pull
+function displayArtistGif(){
+    
+    var search = $(this).attr("data-name");
+    search.toLowerCase();
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q="${search}"&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3`;
+    
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+//select buttons in array to produce giphys
+.then (function(response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var image = $('<img>');
+        var animated = results[i].images.fixed_height.url;
+        var still = results[i].images.fixed_height_still.url;
+        
+        image.attr("src", still);
+        image.attr("data-still", still);
+        image.attr("src", animated);
+        image.attr("data-animate", animated);
+        image.addClass('searchImage');
+        gifDiv.append(p);
+        gifDiv.append(image);
+        
+        //dump the giphy in #gifs area
+        $('.giphyView').prepend(gifDiv);
+      }
+})
+}
+
 
 //create buttons for the array
 
@@ -14,17 +52,17 @@ function renderButtons() {
         
         var a = $('<button>');
         // Adds a class of artist to our button
-        //a.addClass(artist);
+        a.addClass("artist");
         // Added a data-attribute
-        //a.attr('data-name', artists[i]);
-        a.attr("type", "button");
+        a.attr('data-name', artists[i]);
+        //a.attr("type", "button");
         // Provided the initial button text
         a.text(artists[i]);
         // Added the button to the buttons-view div
         $("#buttonArea").append(a);
     }
 }
-renderButtons();
+
 
 //when artist button is pushed
 $('#add-artist').on("click", function(event){
@@ -36,38 +74,7 @@ $('#add-artist').on("click", function(event){
 });
 
 $(document).on('click', '.artist', displayArtistGif);
-
-//giphy api pull
-function displayArtistGif(){
-    var search = $(this).text();
-    var queryURL = `https://api.giphy.com/v1/gifs/search?q="${search}"&api_key=LD6nX1GHMQlF3fLtWdA3FN22QWSLem8n&limit=3`;
-    //`https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-//select buttons in array to produce giphys
-.then (function(response) {
-    for (var i = 0; i < response.data.length; i++) {
-        var gifDiv = $("<div>").addClass('artist');
-        var rating = results[i].rating;
-        var p = $("<p>").text("Rating: " + rating);
-        var animated = response.data[i].images.fixed_height.url;
-        var still = response.data[i].images.fixed_height_still.url;
-        var image = $('<img>');
-        image.attr("src", still);
-        image.attr("data-still", still);
-        image.attr("src", animated);
-        image.attr("data-animate", animated);
-        image.addClass('searchImage');
-        gifDiv.append(p);
-        gifDiv.append(image);
-        
-        //dump the giphy in #gifs area
-        $('.giphyView').append(gifDiv);
-      }
-})
-}
+renderButtons();
 
 //animate/still giphy actions
 $('.giphyView').on('click', 'img',function(){
